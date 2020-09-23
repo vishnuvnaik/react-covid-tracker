7299f06d34f2ef82e78aae13707f241f6f32bca0
vishnu vnaik <vishnuvdev17@gmail.com>
2020-09-23 14:16:13 +0530

    removed console log

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -71,7 +71,6 @@ function LineGraph({ casesType = "cases" }) {
         .then((data) => {
           let chartData = buildChartData(data, casesType);
           setData(chartData);
-          console.log(chartData);
         });
     };
     fetchData();
